165a5711cb17c11475f8eeaef888940ddf03501b
baonguyen96 <baonguyendrump@gmail.com>
2018-04-15 07:46:08 -0500

    Initial commit

---
+++ tf1e.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import tensorflow.contrib.eager as tfe
+tfe.enable_eager_execution()
+
+print(tf.VERSION)
+
+# node1 = tf.constant(3.0)
+# print(node1)
+# print(node1.numpy())
